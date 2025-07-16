fa0f11d17cf8698cbbc72abaa68710712aa2361d
Josh-Medina <joshua.medina2@g.austincc.edu>
2025-07-16 17:28:36 -0500

    exercise-variables assignment

---
+++ exercise-variables-python.py
@@ -0,0 +1,64 @@
+
+
+# 1
+
+onomatopoeia = 'pop'
+print(type(onomatopoeia))
+
+# 2
+
+lyrics = """Katie Casey was baseball mad,   
+Had the fever and had it bad.   
+Just to root for the home town crew,   
+Ev'ry sou   
+Katie blew.   
+On a Saturday her young beau   
+Called to see if she'd like to go   
+To see a show, but Miss Kate said "No,   
+I'll tell you what you can do:"""
+
+# 3
+
+gpa = 3.75
+print(type(gpa))
+
+# 4
+
+empty_var = None
+print(type(empty_var))
+
+# 5
+
+math_value = 828 ** 282
+print(math_value)
+
+# 6 use the calculation or expression for determining the remainder - 0 + 8 + 6, but
+# show in the code the actual variable used to get the answer
+
+math_value2 = (52 - 52) + (64 / 8) + (42 % 8)
+print(math_value2)
+
+# 7
+
+true = True
+isTrue = true != False
+print(isTrue)
+
+# 8 compare 2 values by using the ==,
+# something like is_match = weird_word == “weird”
+
+wierd_word = 'Wierd'
+check = wierd_word == 'wierd'
+print(check)
+
+# 9
+
+#false = false
+
+# 10 
+
+var1 = 35/7
+var2 = 15 ** 3
+var3 = var2 % var1
+var4 = 2 / var1
+print(var3, var4)
